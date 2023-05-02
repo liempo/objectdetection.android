39d5b22c0cd30ec4180c64eddd55fb6bf4ad17f2
Liempo <gonzalesalec@gmail.com>
2023-05-02 17:40:16 +0800

    Add permission checking

--- app/src/main/java/wtf/liempo/objectdetectiontemplate/MainActivity.kt
+++ app/src/main/java/wtf/liempo/objectdetectiontemplate/MainActivity.kt
@@ -3,13 +3,19 @@ package wtf.liempo.objectdetectiontemplate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.rememberPermissionState
 import wtf.liempo.objectdetectiontemplate.ui.theme.ObjectDetectionTemplateTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +23,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ObjectDetectionTemplateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Content()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Content() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CameraStart()
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPermission(
+    permissionState: PermissionState,
+) {
+    PermissionRequired(
+        permissionState = permissionState,
+        permissionNotGrantedContent = { /* ... */ },
+        permissionNotAvailableContent = { /* ... */ }
+    ) {
+        // Open Camera
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraStart() {
+    val permissionState = rememberPermissionState(
+        permission = android.Manifest.permission.CAMERA
+    )
+    CameraPermission(permissionState = permissionState)
+    Button(
+        onClick = {
+            permissionState.launchPermissionRequest()
+        }
+    ) {
+        Text(text = "Open Camera")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ObjectDetectionTemplateTheme {
-        Greeting("Android")
+        Content()
     }
 }
