04ddf405b79fe441706796c4bb45dd65768c7f51
Liempo <gonzalesalec@gmail.com>
2023-05-02 19:42:58 +0800

    Replace colors and fix skeleton UI

--- app/src/main/java/wtf/liempo/objectdetectiontemplate/MainActivity.kt
+++ app/src/main/java/wtf/liempo/objectdetectiontemplate/MainActivity.kt
@@ -7,119 +7,125 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import androidx.compose.ui.tooling.preview.Preview as ComposePreview
+import com.example.compose.AppTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.rememberPermissionState
-import wtf.liempo.objectdetectiontemplate.ui.theme.ObjectDetectionTemplateTheme
+import androidx.compose.ui.tooling.preview.Preview as ComposePreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ObjectDetectionTemplateTheme {
+            AppTheme {
                 MainContent()
             }
         }
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MainContent() {
+    var isShowingCameraPreview by remember { mutableStateOf(false) }
+    val permissionState = rememberPermissionState(
+        permission = android.Manifest.permission.CAMERA
+    )
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            CameraStart()
+        if (isShowingCameraPreview) {
+            CameraPreview(permissionState)
+        } else {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+                    contentDescription = stringResource(id = R.string.content_desc_logo),
+                    modifier = Modifier.fillMaxSize(0.75f)
+                )
+                Button(
+                    onClick = {
+                        if (permissionState.hasPermission) {
+                            isShowingCameraPreview = true
+                        } else {
+                            permissionState.launchPermissionRequest()
+                        }
+                    }
+                ) {
+                    Text(text = "Open Camera")
+                }
+            }
         }
     }
 }
 
+
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun CameraPermission(
-    permissionState: PermissionState,
+fun CameraPreview(
+    permissionState: PermissionState
 ) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
     PermissionRequired(
         permissionState = permissionState,
         permissionNotGrantedContent = { Text(text = "Permission is available") },
         permissionNotAvailableContent = { Text(text = "Permission not available") }
     ) {
-        CameraPreview()
-    }
-}
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                val executor = ContextCompat.getMainExecutor(ctx)
+                cameraProviderFuture.addListener({
+                    val cameraProvider = cameraProviderFuture.get()
 
-@Composable
-fun CameraPreview() {
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { ctx ->
-            val previewView = PreviewView(ctx)
-            val executor = ContextCompat.getMainExecutor(ctx)
-            cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
+                    val preview = Preview.Builder().build().also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
 
-                val preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
-                }
+                    val cameraSelector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
 
-                val cameraSelector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
-
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    cameraSelector,
-                    preview
-                )
-            }, executor)
-            previewView
-        }
-    )
-}
-
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun CameraStart() {
-    val permissionState = rememberPermissionState(
-        permission = android.Manifest.permission.CAMERA
-    )
-//    CameraPermission(permissionState = permissionState)
-    Button(
-        onClick = {
-            permissionState.launchPermissionRequest()
-        }
-    ) {
-        Text(text = "Open Camera")
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview
+                    )
+                }, executor)
+                previewView
+            }
+        )
     }
 }
 
 @ComposePreview(showBackground = true)
 @Composable
 fun DefaultComposePreview() {
-    ObjectDetectionTemplateTheme {
-        CameraPreview()
+    AppTheme {
     }
 }
