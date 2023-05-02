14a9e6b0439070ce8a3a0283831b8ccc5b7c4382
Liempo <gonzalesalec@gmail.com>
2023-05-02 18:59:50 +0800

    Add cameraPreview

--- app/src/main/java/wtf/liempo/objectdetectiontemplate/MainActivity.kt
+++ app/src/main/java/wtf/liempo/objectdetectiontemplate/MainActivity.kt
@@ -3,15 +3,24 @@ package wtf.liempo.objectdetectiontemplate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.compose.ui.tooling.preview.Preview as ComposePreview
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
 import com.google.accompanist.permissions.PermissionState
@@ -23,14 +32,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ObjectDetectionTemplateTheme {
-                Content()
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun Content() {
+fun MainContent() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -51,11 +60,44 @@ fun CameraPermission(
 ) {
     PermissionRequired(
         permissionState = permissionState,
-        permissionNotGrantedContent = { /* ... */ },
-        permissionNotAvailableContent = { /* ... */ }
+        permissionNotGrantedContent = { Text(text = "Permission is available") },
+        permissionNotAvailableContent = { Text(text = "Permission not available") }
     ) {
-        // Open Camera
+        CameraPreview()
     }
+}
+
+@Composable
+fun CameraPreview() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { ctx ->
+            val previewView = PreviewView(ctx)
+            val executor = ContextCompat.getMainExecutor(ctx)
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+
+                val preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    preview
+                )
+            }, executor)
+            previewView
+        }
+    )
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -64,7 +106,7 @@ fun CameraStart() {
     val permissionState = rememberPermissionState(
         permission = android.Manifest.permission.CAMERA
     )
-    CameraPermission(permissionState = permissionState)
+//    CameraPermission(permissionState = permissionState)
     Button(
         onClick = {
             permissionState.launchPermissionRequest()
@@ -74,10 +116,10 @@ fun CameraStart() {
     }
 }
 
-@Preview(showBackground = true)
+@ComposePreview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultComposePreview() {
     ObjectDetectionTemplateTheme {
-        Content()
+        CameraPreview()
     }
 }
